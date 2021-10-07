c3552a80299104bf28badbce3e47a2ae85d2f912
Jovyn Tan <jovyn.tls@gmail.com>
2021-10-08 00:33:28 +0800

    More descriptive error response on uploads

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,10 +1,17 @@
 class UploadsController < ApplicationController
   def index
-    response = {'message' => 'Hello!'}
+    response = { 'message' => 'Hello!' }
     render json: response
   end
 
   def create
-    render json: {'message': 'You have made a POST request!', 'file_size': params[:img].size}
+    if params.nil? || params[:img].nil?
+      payload = { message: '"img" parameter not provided.' }
+      status_code = 400
+    else
+      payload = { message: 'You have made a POST request!', file_size: params[:img].size }
+      status_code = 201
+    end
+    render json: payload, status: status_code
   end
 end
